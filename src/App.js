bc216d39910a6fa12105aa6dfd0a0fe3f6525b6a
Sokrady Chey <sokradychey@gmail.com>
2021-07-29 21:21:55 -0400

    Front End Mentor Form App Project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import Background from './background.png';
+
+function App() {
+  return (
+    <div className='Container'>
+      <div className='Callout'>
+        <h1>Learn to code by watching others</h1>
+        <p>See how experienced developers solve problems in real-time.</p>
+      </div>
+      <div className='Trial'>
+        <p>Try for free for 7 days</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
